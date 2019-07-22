e9ea0fc15eca9c95717a5bff8e8913d70860a315
Sunay Chauhan <SChauhan@nammcal.local>
2019-07-22 11:49:34 -0700

    save work

--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,18 @@ const webpack = require('webpack');
 module.exports = {
     entry: './app/app.js',
     output: {
-        path : path.resolve(__dirname, 'app/dist'), 
-        filename : 'app.bundle.js',
+        path: path.resolve(__dirname, 'app/dist'), 
+        filename: 'app.bundle.js',
+        publicPath: "/dist/"
     },
     devServer: {
         contentBase: path.resolve(__dirname, 'app'),
         publicPath: '/dist/',
-        watchContentBase: true,
+        watchContentBase: false,
         hotOnly: true,
     },
     plugins: [
+        new webpack.NamedModulesPlugin(),
         new webpack.HotModuleReplacementPlugin(),
-
     ]
 };
